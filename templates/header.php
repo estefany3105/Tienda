67cee3e0d9eab676b9827e60ce65a4e416f89535
estefany3105 <142648436+estefany3105@users.noreply.github.com>
2023-08-20 10:42:19 -0500

    header.php

--- templates/header.php
+++ templates/header.php
@@ -13,7 +13,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="css/custom.css">
-
-<script src="js/script.js"></script>
   <body>
   
