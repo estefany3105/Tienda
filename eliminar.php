70b11d92ffd49bf7a08fe881849e5fc56280d5c6
estefany3105 <142648436+estefany3105@users.noreply.github.com>
2023-08-20 14:23:02 -0500

    Update eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -1,41 +1,27 @@
 <?php
 include 'funciones.php';
-
-$config = include 'config.php';
-
+$config = require 'conexion1.php';
 $resultado = [
   'error' => false,
   'mensaje' => ''
 ];
-
-try {
-  $dsn = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'];
-  $conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
   $codigo = $_GET['codigo'];
   $consultaSQL = "DELETE FROM productos WHERE codigo =" . $codigo;
 
-  $sentencia = $conexion->prepare($consultaSQL);
+  $sentencia = $db->prepare($consultaSQL);
   $sentencia->execute();
-
   header('Location: index.php');
-
-} catch(PDOException $error) {
-  $resultado['error'] = true;
-  $resultado['mensaje'] = $error->getMessage();
-}
 ?>
 <html lang="es">
   <head>
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-
     <title>TIENDA TORRES</title>
-
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
 <link rel="stylesheet" href="css/custom.css">
   <body>
 <div class="container mt-2">
@@ -47,5 +33,5 @@ try {
     </div>
   </div>
 </div>
-  </body>
-  </html>
+</body>
+</html>
