a978f4f107b2f09b61dbb9e2acce4f53c219d603
estefany3105 <142648436+estefany3105@users.noreply.github.com>
2023-08-20 11:08:17 -0500

    conectar.php

--- conectar.php
+++ conectar.php
@@ -4,7 +4,7 @@ $config = include 'config.php';
 
 try {
   $conexion = new PDO('mysql:host=' . $config['db']['host'], $config['db']['user'], $config['db']['pass'], $config['db']['options']);
-  $sql = file_get_contents("data/migracion.sql");
+  $sql = file_get_contents("base/base.sql");
 
   $conexion->exec($sql);
 
