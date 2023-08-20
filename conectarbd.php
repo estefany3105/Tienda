dd9784e25394c426afa0168d9d5c8ff0b9fa47c5
estefany3105 <142648436+estefany3105@users.noreply.github.com>
2023-08-20 14:26:58 -0500

    conectarbd.php

--- conectarbd.php
+++ conectarbd.php
@@ -1,9 +1,9 @@
 <?php
 $opciones = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"];
 try{
-    $db = new PDO('mysql:host=localhost;port=3306;dbname=BODEGA','root','',$opciones);
+        $db = new PDO('mysql:host=localhost;port=3306;dbname=TIENDA','root','',$opciones);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conecto servidor primario <br>";
+    echo "Conexion exitosa<br>";
 } catch(PDOException $e){
     echo'¡Upss!Fallo la conexion:' ,$e->getMessage();
 }
