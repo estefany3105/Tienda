bb5c09f9bb3610f781dbac9dbc05c92cf37839aa
estefany3105 <142648436+estefany3105@users.noreply.github.com>
2023-08-20 12:19:10 -0500

    eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -24,9 +24,20 @@ try {
   $resultado['mensaje'] = $error->getMessage();
 }
 ?>
+<html lang="es">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-<?php require "templates/header.php"; ?>
+    <title>TIENDA TORRES</title>
 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/custom.css">
+  <body>
 <div class="container mt-2">
   <div class="row">
     <div class="col-md-12">
@@ -36,5 +47,5 @@ try {
     </div>
   </div>
 </div>
-
-<?php require "templates/footer.php"; ?>
+  </body>
+  </html>
